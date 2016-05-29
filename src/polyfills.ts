82ac6c84a54c4794a58effe6b357fcc36bbab349
Daniel Rasmuson <dan123911@gmail.com>
2016-05-29 12:52:31 -0700

    polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -8,7 +8,6 @@
 // Prefer CoreJS over the polyfills above
 import 'core-js/es6';
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
 
 // Typescript emit helpers polyfill
 import 'ts-helpers';
