2bcaf3a947e759dc1643352abc1d5b3de80b723d
Daniel Rasmuson <dan123911@gmail.com>
2016-05-29 11:58:34 -0700

    trying to add entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,13 @@ module.exports = {
   entry: './src/test.js',
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
   }
 };
